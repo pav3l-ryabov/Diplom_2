19a81b20148c3be9a6d9f49fd22fba1595779979
Pavel Ryabov <pav3l.ryabov@yandex.ru>
2025-05-29 14:59:57 +0300

    Добавили параметризированный тест на создание юзера без необходимых полей

--- data/data.py
+++ data/data.py
@@ -8,3 +8,12 @@ RESPONSE_BODY_EXIST_USER = {
     "success": False,
     "message": "User already exists"
 }
+RESPONSE_BODY_CREATE_USER_WITHOUT_FIELDS = {
+    "success": False,
+    "message": "Email, password and name are required fields"
+}
+DATA_TEST_REG_WITHOUT_FIELDS = [
+        {"email": "email", "password": "", "name": ""},
+        {"email": "", "password": "", "name": "name"},
+        {"email": "email", "password": "", "name": "name"}
+]
--- tests/test_create_user.py
+++ tests/test_create_user.py
@@ -1,6 +1,7 @@
 import allure
+import pytest
 
-from data.data import RESPONSE_BODY_EXIST_USER
+from data.data import RESPONSE_BODY_EXIST_USER, RESPONSE_BODY_CREATE_USER_WITHOUT_FIELDS, DATA_TEST_REG_WITHOUT_FIELDS
 from methods.user_methods import UserMethods
 
 
@@ -28,3 +29,13 @@ class TestCreateUser:
         assert status == 403, f'Ожидали 403, получили {status}'
         assert RESPONSE_BODY_EXIST_USER == response.json(), f'Ответ не соответствует ожидаемому: {response.json()}'
 
+    @allure.title('Тест создания юзера c пустыми полями')
+    @allure.description('Тест создает юзера с пустыми полями, параметризация используется для того, чтобы покрыть проверками'
+                        'все случаи, проверки оптимизированы с помощью техники попарного тестирования')
+    @pytest.mark.parametrize("payload", DATA_TEST_REG_WITHOUT_FIELDS)
+    def test_create_new_user_without_required_field(self, payload):
+        body_payload = payload
+        response, status = UserMethods.create_user(self, body_payload)
+        assert status == 403, f'Ожидали 403, получили {status}'
+        assert RESPONSE_BODY_CREATE_USER_WITHOUT_FIELDS == response.json(), \
+            f'Ответ не соответствует ожидаемому: {response.json()}'
